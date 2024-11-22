b1d95f8f9285361be51e30a43876ed24f71ceaa4
Gabriel Loureiro Amorim Hugi <gabrielhugi2@gmail.com>
2024-11-22 10:45:00 -0300

    Later will add more css

--- JS/BasicFunctions.js
+++ JS/BasicFunctions.js
@@ -16,9 +16,6 @@ Section for fitting the screen properly to the size of the user's screen
 
 -------------------------------------------------------------------------
 */
-
-// calls function resize upon resizing of the screen
-window.addEventListener('resize', resize);
 var screenWidth;
 var screenHeight;
 // calls the function once to set the proper values for the current size of the screen which has not been yet resized
@@ -31,10 +28,12 @@ function resize() {
     document.body.style.width = screenWidth + "px";
     document.body.style.maxWidth = screenWidth + "px";
     // sets minimum heights for style
-    toDoList.style.minHeight = screenHeight*0.65 + "px";
-    doingList.style.minHeight = screenHeight*0.65 + "px";
-    doneList.style.minHeight = screenHeight*0.65 + "px";
+    toDoList.style.height = screenHeight*0.6 + "px";
+    doingList.style.height = screenHeight*0.6 + "px";
+    doneList.style.height = screenHeight*0.6 + "px";
 }
+// calls function resize upon resizing of the screen
+window.addEventListener('resize', resize);
 
 /*
 -------------------------------------------------------------------------
@@ -62,8 +61,17 @@ function addToDo(event) {
         //id is used for identification
         //image is for triggering Expand.js functions
         //<p> is hidden and stores the information of the description
-        toDoList.innerHTML += "<li class='toDoLIReference' title='Date to deliver: " + document.getElementById("newDate").value + "' draggable='true' id='li" + toDoAmmount + "'> <img draggable='false' style='width: 32px; height: 32px; vertical-align: middle;' onclick='expand(this, 0)' id='showli" + toDoAmmount + "' src='../IconsAndImages/info.png'> <input type='checkbox' onclick='toMiddle(this)' id='checkbox" + toDoAmmount + "' class = 'check'>" + newElement.value + "</li>" + "<p class='toDoPLIReference'style='display: none;' id = 'lip" + toDoAmmount + "'>" + newDescription.value + "</p>";
-        //if no date is set then sets the delivery date to "None", which is later translated to 99999999 (for context, non void dates are translated to 8 digit numbers in the format yyyymmdd, for example 20240908)
+        var month_and_day = "";
+        let something = document.getElementById("newDate").value.split('');
+        for (var i = 0; i < 5; i++) {
+            something[i] = "";
+        }
+        for (var i = 0; i < something.length; i++) {
+            month_and_day += something[i];
+        }
+
+        toDoList.innerHTML += "<li class='toDoLIReference' title='Date to deliver: " + document.getElementById("newDate").value + "' draggable='true' id='li" + toDoAmmount + "'> <img draggable='false' style='width: 32px; height: 32px; vertical-align: middle;' onclick='expand(this, 0)' id='showli" + toDoAmmount + "' src='../IconsAndImages/info.png'> <input type='checkbox' onclick='toMiddle(this)' id='checkbox" + toDoAmmount + "' class = 'check'>" + newElement.value + " " + month_and_day + "</li>" + "<p class='toDoPLIReference'style='display: none;' id = 'lip" + toDoAmmount + "'>" + newDescription.value + "</p>";
+        //if no date is set then sets the delivery date to "None", which is later translated to 99999999, in this way it is displayed after all of the other tasks (for context, non void dates are translated to 8 digit numbers in the format yyyymmdd, for example 20240908)
         if (document.getElementById("newDate").value == "") {
             document.getElementById("li" + toDoAmmount).title = "Date to deliver: None";
         }
@@ -94,6 +102,7 @@ function toMiddle(sigma) {
             value = value + fun[i] * Math.pow(10, fun.length - i - 1);
         }
         var title = document.getElementById("li" + value).title;
+
         //adds the information
         //class serves for gathering in Ordering.js
         //title is used to conveniently store the date where the task should be delivered
